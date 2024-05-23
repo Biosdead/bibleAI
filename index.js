be924ef1278add650420b3a282066d97d01b41af
Biosdead <lennonsfurtado@gmail.com>
2024-05-23 18:13:38 -0300

    v0.98_Rute - Fazer 10 imagens por livro.

--- index.js
+++ index.js
@@ -32,6 +32,24 @@ const booksOfBible = [
     "Judas", "Apocalipse"
 ];
 
+const booksOfBibleEnglish = [
+    "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy",
+    "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel",
+    "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra",
+    "Nehemiah", "Esther", "Job", "Psalms", "Proverbs",
+    "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations",
+    "Ezekiel", "Daniel", "Hosea", "Joel", "Amos",
+    "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk",
+    "Zephaniah", "Haggai", "Zechariah", "Malachi",
+    "Matthew", "Mark", "Luke", "John", "Acts",
+    "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians",
+    "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy",
+    "2 Timothy", "Titus", "Philemon", "Hebrews", "James",
+    "1 Peter", "2 Peter", "1 John", "2 John", "3 John",
+    "Jude", "Revelation"
+];
+
+
 
 window.onload = function(){
     populateBookList(); // Chamada da função para preencher o cabeçalho com os nomes dos livros da Bíblia
@@ -43,10 +61,11 @@ window.onload = function(){
     searchBarListener();
 }
 
-function zoom(id){
-    console.log("img" + id);
-    // document.getElementById(""+id).style.transform = "scale(2,2)";
-    openDialog();
+// function zoom(id){
+//     openDialog();
+// }
+
+function translate(){
 
 }
 
@@ -460,7 +479,6 @@ function closeDialog() {
 }
 
 function fullscreen(fonteDaImg,versiculo) {
-    console.log('fullscreen'+fonteDaImg);
     imgSelected.setAttribute('src',fonteDaImg);
     document.getElementById("legenda").innerHTML = versiculo;
     openDialog();
